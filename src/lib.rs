a85af1460f0141a4e695467dc93fa68def81cee3
Tyrone Tudehope <tyronetudehope@gmail.com>
2023-06-13 21:21:04 +0200

    🛠

---
+++ src/lib.rs
@@ -0,0 +1,226 @@
+use crate::operations::{AdjustBrightness, Blur, Crop, Grayscale, Resize};
+use crate::Unit::{Percentage, Pixel};
+use image::io::Reader as ImageReader;
+use image::DynamicImage;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io;
+use std::io::BufWriter;
+use std::ops::{Add, Sub};
+use std::path::Path;
+use thiserror::Error;
+
+pub mod operations;
+
+#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct PixelUnit {
+    pixels: u32,
+}
+
+impl From<PixelUnit> for u32 {
+    fn from(value: PixelUnit) -> Self {
+        value.pixels
+    }
+}
+
+impl From<u32> for PixelUnit {
+    fn from(pixels: u32) -> Self {
+        Self { pixels }
+    }
+}
+
+impl Sub for PixelUnit {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        assert!(self.pixels > rhs.pixels);
+        Self::from(self.pixels - rhs.pixels)
+    }
+}
+
+impl Add for PixelUnit {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::from(self.pixels + rhs.pixels)
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialOrd, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct PercentageUnit {
+    percentage: f32,
+}
+
+#[derive(Error, Clone, Copy, Debug, PartialOrd, PartialEq)]
+#[error("Percentage out of range: {percentage}")]
+pub struct PercentageOutOfRangeError {
+    pub percentage: f32,
+}
+
+impl PercentageOutOfRangeError {
+    fn new(percentage: f32) -> Self {
+        Self { percentage }
+    }
+}
+
+impl TryFrom<f32> for PercentageUnit {
+    type Error = PercentageOutOfRangeError;
+
+    fn try_from(value: f32) -> std::result::Result<Self, Self::Error> {
+        if !(0.0..=1.0).contains(&value) {
+            return Err(PercentageOutOfRangeError::new(value));
+        }
+
+        Ok(Self { percentage: value })
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum Unit {
+    Pixel(PixelUnit),
+    Percentage(PercentageUnit),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Coordinate {
+    x: Unit,
+    y: Unit,
+}
+
+impl Unit {
+    #[inline]
+    fn as_pixel(&self, dimension: PixelUnit) -> PixelUnit {
+        match self {
+            Pixel(pixels) => *pixels,
+            Percentage(percentage) => {
+                let dimension = dimension.pixels as f32;
+                let pixels = dimension * percentage.percentage;
+                PixelUnit::from(pixels as u32)
+            }
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+#[error("Error processing image: {message}")]
+pub struct OperationError {
+    pub message: String,
+}
+
+impl OperationError {
+    fn new(message: String) -> Self {
+        Self { message }
+    }
+}
+
+pub trait Process {
+    fn process(&self, image: DynamicImage) -> Result<DynamicImage, OperationError>;
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum Operation {
+    AdjustBrightness(AdjustBrightness),
+    Blur(Blur),
+    Crop(Crop),
+    Grayscale(Grayscale),
+    Resize(Resize),
+}
+
+impl Operation {
+    fn get_process(&self) -> &dyn Process {
+        match self {
+            Self::AdjustBrightness(adjust) => adjust,
+            Self::Blur(blur) => blur,
+            Self::Crop(crop) => crop,
+            Self::Grayscale(grayscale) => grayscale,
+            Self::Resize(resize) => resize,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum ImageOutputFormat {
+    /// An Image in PNG Format
+    Png,
+    /// An Image in JPEG Format with specified quality, up to 100
+    Jpeg { quality: u8 },
+    // /// An Image in one of the PNM Formats
+    // Pnm(PnmSubtype),
+    /// An Image in GIF Format
+    Gif,
+    /// An Image in ICO Format
+    Ico,
+    /// An Image in BMP Format
+    Bmp,
+    /// An Image in farbfeld Format
+    Farbfeld,
+    /// An Image in TGA Format
+    Tga,
+    /// An Image in OpenEXR Format
+    OpenExr,
+    /// An Image in TIFF Format
+    Tiff,
+    /// An image in AVIF Format
+    Avif,
+    /// An image in QOI Format
+    Qoi,
+    /// An image in WebP Format.
+    WebP,
+}
+
+impl From<ImageOutputFormat> for image::ImageOutputFormat {
+    fn from(value: ImageOutputFormat) -> Self {
+        match value {
+            ImageOutputFormat::Png => Self::Png,
+            ImageOutputFormat::Jpeg { quality } => Self::Jpeg(quality),
+            ImageOutputFormat::Gif => Self::Gif,
+            ImageOutputFormat::Ico => Self::Ico,
+            ImageOutputFormat::Bmp => Self::Bmp,
+            ImageOutputFormat::Farbfeld => Self::Farbfeld,
+            ImageOutputFormat::Tga => Self::Tga,
+            ImageOutputFormat::OpenExr => Self::OpenExr,
+            ImageOutputFormat::Tiff => Self::Tiff,
+            ImageOutputFormat::Avif => Self::Avif,
+            ImageOutputFormat::Qoi => Self::Qoi,
+            ImageOutputFormat::WebP => Self::WebP,
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    OperationError(#[from] OperationError),
+
+    #[error(transparent)]
+    PercentageOutOfRangeError(#[from] PercentageOutOfRangeError),
+
+    #[error("IO error")]
+    IoError(#[from] io::Error),
+
+    #[error("Image error")]
+    ImageError(#[from] image::ImageError),
+}
+
+pub fn process_file<P: AsRef<Path>>(
+    in_path: P,
+    out_path: P,
+    out_format: ImageOutputFormat,
+    operations: Vec<Operation>,
+) -> Result<(), Error> {
+    let mut image = ImageReader::open(in_path)?.decode()?;
+
+    for operation in operations.into_iter() {
+        image = operation.get_process().process(image)?;
+    }
+
+    let out_file = File::create(out_path)?;
+    let mut out_buf = BufWriter::new(out_file);
+    image.write_to(&mut out_buf, out_format)?;
+
+    Ok(())
+}
---
+++ src/operations/crop.rs
@@ -0,0 +1,248 @@
+use crate::{Coordinate, OperationError, PixelUnit, Process};
+
+use image::{DynamicImage, GenericImageView};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Crop {
+    pub from: Coordinate,
+    pub to: CropOrigin,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum CropOrigin {
+    Minimum(Coordinate),
+    Maximum(Coordinate),
+    CropStart(Coordinate),
+}
+
+impl CropOrigin {
+    fn as_pixel_coordinate(
+        &self,
+        x: PixelUnit,
+        y: PixelUnit,
+        width: PixelUnit,
+        height: PixelUnit,
+    ) -> (PixelUnit, PixelUnit) {
+        match self {
+            Self::Minimum(coordinate) => {
+                (coordinate.x.as_pixel(width), coordinate.y.as_pixel(height))
+            }
+            Self::Maximum(coordinate) => {
+                let x = width - coordinate.x.as_pixel(width);
+                let y = height - coordinate.y.as_pixel(height);
+                (x, y)
+            }
+            Self::CropStart(coordinate) => {
+                let x = x + coordinate.x.as_pixel(width);
+                let y = y + coordinate.y.as_pixel(height);
+                (x, y)
+            }
+        }
+    }
+}
+
+impl Process for Crop {
+    fn process(&self, image: DynamicImage) -> Result<DynamicImage, OperationError> {
+        let (width, height) = image.dimensions();
+        let width = PixelUnit::from(width);
+        let height = PixelUnit::from(height);
+
+        let left = self.from.x.as_pixel(height);
+        let top = self.from.y.as_pixel(width);
+
+        let (right, bottom) = self.to.as_pixel_coordinate(left, top, width, height);
+
+        if bottom < top {
+            return Err(OperationError::new(format!(
+                "Bottom cannot be less than top for crop operation {self:?}"
+            )));
+        }
+
+        if right < left {
+            return Err(OperationError::new(format!(
+                "Right cannot be less than left for crop operation {self:?}"
+            )));
+        }
+
+        Ok(image.crop_imm(
+            left.into(),
+            top.into(),
+            (right - left).into(),
+            (bottom - top).into(),
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::operations::crop::CropOrigin;
+    use crate::{Coordinate, Unit};
+
+    const CANVAS_WIDTH: u32 = 100;
+    const CANVAS_HEIGHT: u32 = 100;
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_minimum_pixel() {
+        let crop_origin = CropOrigin::Minimum(Coordinate {
+            x: Unit::Pixel(10.into()),
+            y: Unit::Pixel(10.into()),
+        });
+
+        assert_eq!(
+            (10.into(), 10.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_minimum_percent() {
+        let crop_origin = CropOrigin::Minimum(Coordinate {
+            x: Unit::Percentage(0.8.try_into().unwrap()),
+            y: Unit::Percentage(0.8.try_into().unwrap()),
+        });
+
+        assert_eq!(
+            (80.into(), 80.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_minimum_mixed() {
+        let crop_origin = CropOrigin::Minimum(Coordinate {
+            x: Unit::Percentage(0.8.try_into().unwrap()),
+            y: Unit::Pixel(50.into()),
+        });
+
+        assert_eq!(
+            (80.into(), 50.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_maximum_pixel() {
+        let crop_origin = CropOrigin::Maximum(Coordinate {
+            x: Unit::Pixel(10.into()),
+            y: Unit::Pixel(10.into()),
+        });
+
+        assert_eq!(
+            (90.into(), 90.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_maximum_percent() {
+        let crop_origin = CropOrigin::Maximum(Coordinate {
+            x: Unit::Percentage(0.2.try_into().unwrap()),
+            y: Unit::Percentage(0.2.try_into().unwrap()),
+        });
+
+        assert_eq!(
+            (80.into(), 80.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_maximum_mixed() {
+        let crop_origin = CropOrigin::Maximum(Coordinate {
+            x: Unit::Percentage(0.2.try_into().unwrap()),
+            y: Unit::Pixel(50.into()),
+        });
+
+        assert_eq!(
+            (80.into(), 50.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_cropstart_pixel() {
+        let crop_origin = CropOrigin::CropStart(Coordinate {
+            x: Unit::Pixel(10.into()),
+            y: Unit::Pixel(10.into()),
+        });
+
+        assert_eq!(
+            (15.into(), 15.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_cropstart_percent() {
+        let crop_origin = CropOrigin::CropStart(Coordinate {
+            x: Unit::Percentage(0.2.try_into().unwrap()),
+            y: Unit::Percentage(0.2.try_into().unwrap()),
+        });
+
+        assert_eq!(
+            (25.into(), 25.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+
+    #[test]
+    fn crop_origin_as_pixel_coordinate_cropstart_mixed() {
+        let crop_origin = CropOrigin::CropStart(Coordinate {
+            x: Unit::Percentage(0.2.try_into().unwrap()),
+            y: Unit::Pixel(50.into()),
+        });
+
+        assert_eq!(
+            (25.into(), 55.into()),
+            crop_origin.as_pixel_coordinate(
+                5.into(),
+                5.into(),
+                CANVAS_WIDTH.into(),
+                CANVAS_HEIGHT.into(),
+            )
+        );
+    }
+}
